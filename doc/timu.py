4df86b22129acc550927f6ef8ec6a2b2e9e41504
Xuefeng Wu <benewu@gmail.com>
2012-05-08 14:44:06 +0800

    fix: count scoap

--- doc/timu.py
+++ doc/timu.py
@@ -6,10 +6,11 @@ from sets import Set
 
 timu = r'<div class=".*">第[0-9]+题:<p>(.*)</p>'
 
-timus = Set(['']) 
 
+sum = 0
 names = {"综合","采购","成本","道德","范围","风险","概述","沟通","人力","时间","整合","质量"}
 for name in names:	
+	timus = Set(['']) 
 	path="C:\\Users\\19002850.CORP\\Documents\\pmpway\\"+name  # insert the path to the directory of interest
 	dirList=os.listdir(path)
 	for fname in dirList:
@@ -24,7 +25,9 @@ for name in names:
 			if m is not None:
 				timus.add(m.group(1)) 		
 			line = fileIN.readline() 
-		
+	sum += len(timus)	
 	print name + ": "+ str(len(timus)) + "/"+ str(len(dirList)) + "="+ str((len(timus)*100)/(len(dirList)*15))
+	
+print sum	
 	
  	
