a7d24a127440fec59b4a266dbaa20415f231c9a1
Xuefeng.Wu <benewu@gmail.com>
2012-12-25 19:11:42 +0800

    scala macros sample

---
+++ acrawler/src/main/scala/sample/hello/printf.scala
@@ -0,0 +1,35 @@
+package sample.hello
+
+import language.experimental.macros
+import scala.reflect.macros.Context
+import scala.collection.mutable.{ ListBuffer, Stack }
+
+object PrintfMacros {
+  def printf(format: String, params: Any*): Unit = macro printf_impl
+
+  //macro definition
+  def printf_impl(c: Context)(format: c.Expr[String], params: c.Expr[Any]*): c.Expr[Unit] = {
+    //it includes a lot of routinely used functions and types
+    import c.universe._
+    //parse the provided format string.
+    val Literal(Constant(s_format: String)) = format.tree
+    val evals = ListBuffer[ValDef]()
+    def precompute(value: Tree, tpe: Type): Ident = {
+      val freshName = newTermName(c.fresh("eval$"))
+      evals += ValDef(Modifiers(), freshName, TypeTree(tpe), value)
+      Ident(freshName)
+    }
+    val paramsStack = Stack[Tree]((params map (_.tree)): _*)
+    val refs = s_format.split("(?<=%[\\w%])|(?=%[\\w%])") map {
+      case "%d" => precompute(paramsStack.pop, typeOf[Int])
+      case "%s" => precompute(paramsStack.pop, typeOf[String])
+      case "%%" => Literal(Constant("%"))
+      case part => Literal(Constant(part))
+    }
+    // create ASTs (abstract syntax trees) which represent Scala code. 
+    // reify provides a shortcut for creating ASTs
+    val stats = evals ++ refs.map(ref => reify(print(c.Expr[Any](ref).splice)).tree)
+    // combines all the generated code into a Block
+    c.Expr[Unit](Block(stats.toList, Literal(Constant(()))))
+  }
+}
