f7198ce39e9391d9d2e1a09d92cca85282b46e25
Xuefeng Wu <benewu@gmail.com>
2013-05-31 18:04:43 +0800

    update play version for fix scala version

--- scala123/project/Build.scala
+++ scala123/project/Build.scala
@@ -4,16 +4,17 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "website"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "scala123"
+  val appVersion      = "1.0-SNAPSHOT"  
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+  scalaVersion := "2.10.1" 
+
+
+
+  val common = Project("common", file("common")).settings(
+    // Add your own project settings here    
+    scalaVersion := "2.10.1" 
   )
-
-  val common = Project("common", file("common"))
 
   val crawler = Project("crawler", file("crawler")).dependsOn(common)
 	
@@ -25,9 +26,15 @@ object ApplicationBuild extends Build {
     appName + "-admin", appVersion, path = file("admin")
   ).dependsOn(common)
 
+  val mainDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
+
   val main = play.Project(appName + "-main", appVersion, appDependencies, path = file("website")).settings(
     // Add your own project settings here    
-    scalaVersion := "2.10.1"   
+
   ).dependsOn(common,crawler,news,adminArea)
 
 }
